fc09f544977e12abdf0c9d38a2b95d293f9f038e
shravanrn@gmail.com <shravanrn@gmail.com>
2021-08-11 11:24:19 -0700

    Minor script/build fixes

--- build-lucet/run.sh
+++ build-lucet/run.sh
@@ -23,7 +23,7 @@ else
   ASLR_FLAGS=
 fi
 
-$HOME/spectresandboxing/lucet-spectre/target/release/lucet-wasi \
+../../lucet-spectre/target/release/lucet-wasi \
     --heap-address-space "8GiB" \
     --max-heap-size "4GiB" \
     --stack-size "128MiB" \
